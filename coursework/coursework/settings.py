1f15f47377fc0cb07d9d82235f6d75b897fd9ff2
nastuaah <homenko.nastya2004@yandex.ru>
2025-05-15 08:45:02 +0300

    Changes in settings

--- coursework/coursework/settings.py
+++ coursework/coursework/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     }
 }
 
-WSGI_APPLICATION = 'coursework.wsgi.application'
+WSGI_APPLICATION = 'coursework.coursework.wsgi.application'
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
